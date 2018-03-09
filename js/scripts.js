bed21d57b64e76350d1ceb9391d406d8f4d5c632
Carlos Cordero <carlosdcordero@gmail.com>
2018-03-09 09:36:14 -0500

    Corrección de archivos

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
+// Pre-cargar imágenes
+$.preloadImages = function() {
+    for(var i = 0; i<arguments.length; i++) {
+        $("<img />").attr("src", arguments[i]);
+    }
+}
+$(document).ready(function() {
+    $.preloadImages(
+        "carlos-cordero.jpg",
+        "adrielle-inc.jpg",
+        "dolce-angel.jpg",
+        "es-moda.jpg",
+        "maria-rondon.jpg",
+        "mariachi-tequila-show.jpg",
+        "mector.jpg"
+    );
+});
+
 // WOW
-new WOW().init();
+$(document).ready(function() {
+    new WOW().init();
+});
 
 // Animación scroll
 $(document).ready(function() {
-  $('a[href^="#"]').click(function(){
-    var link = $(this);
-    var anchor  = link.attr('href');
-    $('html, body').stop().animate({
-      scrollTop: jQuery(anchor).offset().top
-    }, 1000);
-    return false;
-  });
+    $('a[href^="#"]').click(function(){
+        var link = $(this);
+        var anchor  = link.attr('href');
+        $('html, body').stop().animate({
+        scrollTop: jQuery(anchor).offset().top
+        }, 1000);
+        return false;
+    });
 });
 
 // Ocultar botón arriba
